de53019c8584798d506d7dfa4a194ffdfadb8fc0
TzuHanLiang <crocodilestear@gmail.com>
2019-12-19 13:38:52 +0800

    css style

--- build/frontend/assets/js/script.js
+++ build/frontend/assets/js/script.js
@@ -1,8 +1,10 @@
 const els = {
   tab: document.querySelector(".tab"),
-  dropdownbtns: document.querySelectorAll(".dropdown--btn"),
+  dropdownBtns: document.querySelectorAll(".dropdown--btn"),
+  backgroundBtn: document.querySelector(".background__btn"),
+  backgroundCheckbox: document.querySelector(".background__checkbox"),
   tablinks: document.querySelectorAll(".tab__links"),
-  btnCems: document.querySelector('.btn-cems'),
+  btnCems: document.querySelector(".btn-cems")
 };
 
 const openDropdown = evt => {
@@ -13,12 +15,50 @@ const openDropdown = evt => {
 const openTabContent = evt => {
   const className = evt.target.dataset.type;
   els.tab.classList = [`tab ${className}`];
-}
+};
 
-Array.from(els.dropdownbtns).forEach(dropdownbtn =>
-  dropdownbtn.addEventListener("click", openDropdown, false)
+const closeBackgroundSub = _ => {
+  console.log("click")
+  els.backgroundCheckbox.checked = false;
+};
+
+els.backgroundBtn.addEventListener("click", closeBackgroundSub, false);
+
+Array.from(els.dropdownBtns).forEach(dropdownBtn =>
+  dropdownBtn.addEventListener("click", openDropdown, false)
 );
 
 Array.from(els.tablinks).forEach(tablink =>
   tablink.addEventListener("click", openTabContent, false)
 );
+
+
+//===================
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
